1fe393d7e05f0d205217a3aa04155b5e4a6b093f
zlobbi <kadyrbek.mavlyanov@gmail.com>
2020-03-28 11:07:04 +0600

    Initial commit

---
+++ src/main/java/km/exam7/foodorders/FoodordersApplication.java
@@ -0,0 +1,13 @@
+package km.exam7.foodorders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodordersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodordersApplication.class, args);
+    }
+
+}
---
+++ src/test/java/km/exam7/foodorders/FoodordersApplicationTests.java
@@ -0,0 +1,13 @@
+package km.exam7.foodorders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodordersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
